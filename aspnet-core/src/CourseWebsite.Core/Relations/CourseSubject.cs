791ec833f90fd0ce7f494f5fce7863e687665ee0
Đinh Công Thành <cngthnh22112002@gmail.com>
2024-10-12 23:50:03 +0700

    feat: add new join table 'CourseSubject' between Course and Subject

---
+++ aspnet-core/src/CourseWebsite.Core/Relations/CourseSubject.cs
@@ -0,0 +1,17 @@
+﻿using Abp.Domain.Entities.Auditing;
+using CourseWebsite.Courses;
+using CourseWebsite.Subjects;
+
+namespace CourseWebsite.Relations
+{
+    public class CourseSubject : FullAuditedEntity<int>
+    {
+        public int CourseId { get; set; }
+        public int SubjectId { get; set; }
+        public int Order { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual Subject Subject { get; set; }
+
+    }
+}

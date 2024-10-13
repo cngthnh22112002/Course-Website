9a661af7142e62be888422870ca0a5e20adcd334
Đinh Công Thành <cngthnh22112002@gmail.com>
2024-10-13 15:22:45 +0700

    feat: add interface and implementation for Subject CRUD operations

---
+++ aspnet-core/src/CourseWebsite.Application/Subjects/Services/Interfaces/ISubjectAppService.cs
@@ -0,0 +1,9 @@
+﻿using Abp.Application.Services;
+using CourseWebsite.Subjects.Dtos;
+
+namespace CourseWebsite.Subjects.Services.Interfaces
+{
+    public interface ISubjectAppService : IAsyncCrudAppService<SubjectDto, int, SubjectFilterDto, CreateSubjectDto, SubjectDto>
+    {
+    }
+}

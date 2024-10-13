9021b3128ae525dd3f46a08f9cf4ef1c78949ead
Đinh Công Thành <cngthnh22112002@gmail.com>
2024-10-13 15:21:53 +0700

    feat: add DTOs for Subject CRUD operations

---
+++ aspnet-core/src/CourseWebsite.Application/Subjects/Dtos/SubjectFilterDto.cs
@@ -0,0 +1,10 @@
+﻿using Abp.Application.Services.Dto;
+
+namespace CourseWebsite.Subjects.Dtos
+{
+    public class SubjectFilterDto : PagedAndSortedResultRequestDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
